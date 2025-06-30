f72128eb291e724461ce63cd2e6869f90d5d42ec
ArianaNChaves <navi.ariana99@gmail.com>
2025-06-30 14:21:15 -0300

    Add tutorial menu

--- Assets/Scripts/Utilities/PrefabsManager.cs
+++ Assets/Scripts/Utilities/PrefabsManager.cs
@@ -7,8 +7,11 @@ public class PrefabsManager : MonoBehaviour
 {
     [SerializeField] private Enemy normalEnemyPrefab;
     [SerializeField] private int normalEnemyPrefabSize;
+    private bool _isPoolInitialized = false;
     void Start()
     {
+        if (_isPoolInitialized) return;
+        _isPoolInitialized = true;
         PoolManager.Instance.InitializePool(normalEnemyPrefab, normalEnemyPrefabSize);
     }
 
